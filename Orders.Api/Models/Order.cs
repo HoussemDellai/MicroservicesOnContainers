d8d08a565ac53f54831a19f50d233b2f8ba8e66f
Houssem Dellai <houssem.dellai@soat.fr>
2018-12-21 08:11:02 +0100

    Added orders project

---
+++ Orders.Api/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orders.Api.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        
+        public List<int> ProductsId { get; set; }
+
+        public double TotalPrice { get; set; }
+    }
+}
