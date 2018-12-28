0c6f66c4352482c73cb3c88f3c0109b24b077993
Houssem Dellai <houssem.dellai@ageas.fr>
2018-12-28 08:04:18 +0100

    Updated projects to .NET Core 2.2

--- Frontend.Mvc/Startup.cs
+++ Frontend.Mvc/Startup.cs
@@ -28,7 +28,7 @@ namespace Frontend.Mvc
             });
             
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
