163c5f1ba0773dc67335fa64396fc28dcee33032
Houssem Dellai <houssem.dellai@ageas.fr>
2019-01-06 11:34:25 +0100

    code and ui cleanup

--- Frontend.Mvc/Controllers/BasketItemsController.cs
+++ Frontend.Mvc/Controllers/BasketItemsController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Frontend.Mvc.Models;
 using System.Net.Http;
 using Newtonsoft.Json;
@@ -14,14 +12,12 @@ namespace Frontend.Mvc.Controllers
 {
     public class BasketItemsController : Controller
     {
-        IConfiguration _configuration;
         private readonly string _apiGatewayUrl;
         private readonly FrontendMvcContext _context;
 
         public BasketItemsController(IConfiguration configuration)
         {
-            _configuration = configuration;
-            _apiGatewayUrl = _configuration.GetValue<string>("ApiGatewayUrl");
+            _apiGatewayUrl = configuration.GetValue<string>("ApiGatewayUrl");
             _apiGatewayUrl = "http://104.45.20.246";
             Console.WriteLine($"_apiGatewayUrl : {_apiGatewayUrl}");
             _context = null;
@@ -62,131 +58,6 @@ namespace Frontend.Mvc.Controllers
             var basketItems = JsonConvert.DeserializeObject<List<BasketItem>>(json);
 
             return View(basketItems);
-        }
-
-        // GET: BasketItems/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var basketItem = await _context.BasketItem
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (basketItem == null)
-            {
-                return NotFound();
-            }
-
-            return View(basketItem);
-        }
-
-        // GET: BasketItems/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: BasketItems/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ProductId,Name")] BasketItem basketItem)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(basketItem);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(basketItem);
-        }
-
-        // GET: BasketItems/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var basketItem = await _context.BasketItem.FindAsync(id);
-            if (basketItem == null)
-            {
-                return NotFound();
-            }
-            return View(basketItem);
-        }
-
-        // POST: BasketItems/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductId,Name")] BasketItem basketItem)
-        {
-            if (id != basketItem.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(basketItem);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BasketItemExists(basketItem.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(basketItem);
-        }
-
-        // GET: BasketItems/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var basketItem = await _context.BasketItem
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (basketItem == null)
-            {
-                return NotFound();
-            }
-
-            return View(basketItem);
-        }
-
-        // POST: BasketItems/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var basketItem = await _context.BasketItem.FindAsync(id);
-            _context.BasketItem.Remove(basketItem);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool BasketItemExists(int id)
-        {
-            return _context.BasketItem.Any(e => e.Id == id);
         }
     }
 }
--- Frontend.Mvc/Controllers/HealthCheckController.cs
+++ Frontend.Mvc/Controllers/HealthCheckController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Frontend.Mvc.Models;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 
 namespace Frontend.Mvc.Controllers
 {
