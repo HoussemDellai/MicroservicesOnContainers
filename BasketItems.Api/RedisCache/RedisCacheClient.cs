4c4b89168cee666000f08305ab70e65598d92a9d
Houssem <houssem@Houssem.local>
2019-01-12 15:41:37 +0100

    added package Redis Cache

---
+++ BasketItems.Api/RedisCache/RedisCacheClient.cs
@@ -0,0 +1,21 @@
+using StackExchange.Redis;
+
+namespace Basket.Api
+{
+    public class RedisCacheClient
+{
+    private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = Configuration[SecretName];
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+}
+}
